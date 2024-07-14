56a63eb3ec2f2aeebc5986d237da185fdea98044
PitrPi <pitrpicko+git@gmail.com>
2024-07-14 19:21:47 +0100

    726 regex and rolling multiplier

--- 726/solution.py
+++ 726/solution.py
@@ -1,85 +1,49 @@
+import re
 from collections import Counter, defaultdict, deque
 
 
 class Solution:
     def countOfAtoms(self, formula: str) -> str:
-        c = self.count_complex(formula)
-        res = ""
-        for key, value in sorted(c.items()):
-            res += key
-            if value > 1:
-                res += str(value)
-        return res
-
-    def get_multiplicator(self, string):
-        multiplicator = ""
-        brackets = 1
-        for s in string:
-            if brackets == 0 and s.isnumeric():
-                multiplicator += s
-            elif s == "(" and multiplicator == "":
-                brackets += 1
-            elif s == ")" and multiplicator == "":
-                brackets -= 1
-            elif brackets == 0:
-                break
-
-        return int(multiplicator) if multiplicator else 1
-
-    def count_complex(self, string: str) -> Counter:
-        res = Counter()
-        for idx, s in enumerate(string):
-            if s == "(":
-                res += self.count_simple(string[:idx])
-                comple = self.count_complex(string[idx + 1 :])
-                multiplicator = self.get_multiplicator(string[idx + 1 :])
-                if comple:
-                    for key, value in comple.items():
-                        comple[key] = value * multiplicator
-                    res += comple
-                return res
-            elif s == ")":
-                simple = self.count_simple(string[:idx])
-                # multiplicator = self.get_multiplicator(string[idx:])
-                # for key, value in simple.items():
-                #     simple[key] = value * multiplicator
-                res += simple
-                res += self.count_complex(string[idx + 1 :])
-                return res
-        return res
-
-    def count_simple(self, string: str) -> Counter:
+        eles = re.findall(r"[A-Z][a-z]*|\d+|\(|\)", formula)
         c = Counter()
-        c[""] = 0
-        stack = ""
-        latest = ""
-        for idx, s in enumerate(string):
+        multipliers = [1]
+        brackets = 0
+        coupled = False
+        for idx in range(len(eles) - 1, -1, -1):
+            if eles[idx].isnumeric():
+                multipliers.append(int(eles[idx]))
+                coupled = True
+            elif eles[idx].isalpha():
+                mult = 1
+                for i in range(brackets + int(coupled)):
+                    try:
+                        mult *= multipliers[-i - 1]
+                    except IndexError:
+                        break
+                c[eles[idx]] += mult
+                if coupled:
+                    multipliers.pop()
+                coupled = False
+            elif eles[idx] == ")":
+                brackets += 1
+                coupled = False
+            elif eles[idx] == "(":
+                brackets -= 1
+                multipliers.pop()
+                coupled = False
 
-            if ord(s) >= 97:
-                stack += s
-            elif s.isnumeric():
-                if stack.isnumeric():
-                    stack += s
-                else:
-                    latest = stack
-                    stack = s
-            else:
-                if stack.isnumeric():
-                    c[latest] += int(stack)
-                    stack = s
-                else:
-                    c[stack] += 1
-                    stack = s
-        if stack:
-            if stack.isnumeric():
-                c[latest] += int(stack)
-            else:
-                c[stack] += 1
-        c.pop("")
-        return c
+            if not multipliers:
+                multipliers.append(1)
+        res = ""
+        for ele, count in sorted(c.items()):
+            res += ele
+            if count > 1:
+                res += str(count)
+        return res
 
 
 if __name__ == "__main__":
     Solution().countOfAtoms("Mg((AB)3(CD)2)2")
     Solution().countOfAtoms("HaH2O3")
     Solution().countOfAtoms("Mg(OH)2")
+    Solution().countOfAtoms("K4(ON(SO3)2)2")
